9d752da033d1750f88ce70e870df2e576436f43b
Charles Eddy <charles.eddy@dell.com>
2024-12-11 10:26:55 -0700

    demo code upload

---
+++ get_jenkins.sh
@@ -0,0 +1,3 @@
+wget -p -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
